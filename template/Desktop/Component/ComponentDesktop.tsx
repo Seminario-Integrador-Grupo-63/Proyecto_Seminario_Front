2ca05e322bdcbb9c6ae4181ed22a4f1948407892
JCaeta <63880187+JCaeta@users.noreply.github.com>
2023-08-16 11:22:44 -0300

    React template - Storybook + Next.js + Axios + TypeScript

---
+++ template/Desktop/Component/ComponentDesktop.tsx
@@ -0,0 +1,21 @@
+import styles from './ComponentDesktop.module.scss';
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const Component = (props: any) => {
+    return (<>
+
+    </>);
+}
+
+ComponentDesktop.defaultProps =
+{
+
+}
+
+ComponentDesktop.propTypes = 
+{
+
+}
+
+
