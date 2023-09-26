9e6fe007e7d91204a50652a407e2d9d1baad29ce
feduZab <feduzabala@gmail.com>
2023-09-26 13:01:42 -0300

    DishCard
    DishCard

--- qresto-components/templates/Components/Component.tsx
+++ qresto-components/templates/Components/Component.tsx
@@ -1,21 +1,22 @@
-import styles from './Component.module.scss';
 import React from 'react';
 import PropTypes from 'prop-types';
 
+
 export const Component = (props: any) => {
     return (<>
-
+        
     </>);
 }
 
 Component.defaultProps =
 {
-
+   
+    
 }
 
 Component.propTypes = 
 {
-
+    
 }
 
 
