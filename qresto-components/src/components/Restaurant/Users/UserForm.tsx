a46492fa3957c3838ddbc010bd88a89351eceabb
Harumi236 <harumi236@proton.me>
2024-01-24 14:41:32 -0300

    Modificado para que funcione con props, agregado boton para ocultar/ver password,queda pendiente corregir modelo de usuario y agregar funcionalidad necearia para abrir y cerrar xD

--- qresto-components/src/components/Restaurant/Users/UserForm.tsx
+++ qresto-components/src/components/Restaurant/Users/UserForm.tsx
@@ -1,19 +1,24 @@
-import {Grid, InputLabel, MenuItem, Select, TextField} from "@mui/material";
+import {Grid, IconButton, InputAdornment, MenuItem, TextField} from "@mui/material";
 import React, {useState} from "react";
 import PropTypes from "prop-types";
 import {FormDialog} from "@/Common/FormDialog";
+import {Visibility, VisibilityOff} from "@mui/icons-material";
 
 
 export  const UserForm = (props: any) => {
 
+    const [showPassword, setShowPassword] = React.useState(false);
 
+    const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+    const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault();
+    };
     const [formData, setFormData] = useState({
-        selectedOption: '',
-        selectedCategory: '',
-        inputValueUsername: '',
-        inputValueEmail: '',
-        inputValuePassword: '',
-        selectedRole: '',
+        inputValueUsername: props.user.username,
+        inputValueEmail: props.user.email,
+        inputValuePassword: props.user.password,
+        selectedRole: props.user.role,
     });
 
     return (<>
@@ -26,62 +31,69 @@ export  const UserForm = (props: any) => {
             closeText={props.closeText}
         >
             <Grid container spacing={2}>
+
                 <Grid item xs={12} sm={6} >
                     <TextField
+                        margin={"dense"}
                         label="Nombre de Usuario"
                         name="inputValueUser"
                         variant="outlined"
                         aria-readonly={props.readOnly}
-                        defaultValue={props.user.username}
                         fullWidth
                         value={formData.inputValueUsername}
                         onChange={(e) => setFormData({ ...formData, inputValueUsername: e.target.value })}
                     >
                     </TextField>
                 </Grid>
+
                 <Grid item xs={12} sm={6} >
                     <TextField
+                        margin={"dense"}
                         label="Email"
                         name="inputValueEmail"
                         variant="outlined"
                         aria-readonly={props.readOnly}
-                        defaultValue={props.user.email}
                         fullWidth
                         value={formData.inputValueEmail}
                         onChange={(e) => setFormData({ ...formData, inputValueEmail: e.target.value })}
                     >
                     </TextField>
                 </Grid>
+
+
                 <Grid item xs={12} sm={6} >
                     <TextField
+                        type={showPassword ? 'text' : 'password'}
+                        InputProps={{endAdornment:
+                            <InputAdornment position="end">
+                                <IconButton
+                                    aria-label="toggle password visibility"
+                                    onClick={handleClickShowPassword}
+                                    onMouseDown={handleMouseDownPassword}
+                                    edge="end"
+                                >
+                                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                                </IconButton>
+                            </InputAdornment>}}
+                        margin={"dense"}
                         label="Password"
                         name="inputValuePassword"
                         variant="outlined"
-/*
-                        aria-readonly={props.readOnly}
-*/
-/*
-                        defaultValue={props.user.password}
-*/
                         fullWidth
                         value={formData.inputValuePassword}
                         onChange={(e) => setFormData({ ...formData, inputValuePassword: e.target.value })}
                     >
                     </TextField>
                 </Grid>
+
                 <Grid item xs={12} sm={6}>
-                    <InputLabel id="role-label">Rol</InputLabel>
-                    <Select
-                        labelId="role-label"
+                    <TextField
+                        margin={"dense"}
+                        select
                         id="role-selection"
                         label="Rol"
                         name="selected-role"
                         variant="outlined"
-/*                        aria-readonly={props.readOnly}
-                        readOnly={props.readOnly}*/
-/*
-                        defaultValue={props.user.role}
-*/
                         fullWidth
                         value={formData.selectedRole}
                         onChange={(e) => setFormData({ ...formData, selectedRole: e.target.value })}
@@ -89,7 +101,7 @@ export  const UserForm = (props: any) => {
                     >
                         <MenuItem value={"mozo"}>Mozo</MenuItem>
                         <MenuItem value={"admin"}>Admin</MenuItem>
-                    </Select>
+                    </TextField>
                 </Grid>
 
             </Grid>
@@ -100,19 +112,19 @@ export  const UserForm = (props: any) => {
 }
 
 
-UserForm.DefaultProps = {
+UserForm.defaultProps = {
     submitText: "Confirmar",
     closeText: "Cancelar",
     onSubmit: function () {},
     open: false,
     onClose: function (){},
-    readOnly: false,
+    readOnly: true,
     user: {username: "Johhny", email: "dwad"},
-    title: "Form",
+    title: "Detalles de Usuario",
 
 }
 
-UserForm.PropTypes = {
+UserForm.propTypes = {
     submitText: PropTypes.string,
     closeText: PropTypes.string,
     onSubmit: PropTypes.func,
