be739bc8b997592033b7ced0fce2d137e1c56c54
Harumi236 <harumi236@proton.me>
2024-01-25 16:29:22 -0300

    Comenzando UpdatePrices

--- qresto-restaurant/src/pages/foodmenu/UpdatePrices/index.tsx
+++ qresto-restaurant/src/pages/foodmenu/UpdatePrices/index.tsx
@@ -1,10 +1,28 @@
 import UpdatePrices from "@/Restaurant/UpdatePrices/UpdatePrices";
+import {useState} from "react";
+import {getCategories} from "@/requests";
 
 
 export default function UpdatePricesPage() {
 
+    const [categoryOptions, setCategoryOptions] = useState()
+    const [updateOptions, setUpdateOptions] = useState(['Aumentar', 'Disminuir'])
+    const [productList, setProductList] = useState()
 
-    return (<></>
+
+
+    const fetchCategories = async (restaurantId) => {
+        const fetchedCategories = await getCategories()
+        setCategoryOptions(fetchedCategories)
+    }
+
+
+
+    return (<UpdatePrices
+            categoryOptions={categoryOptions}
+            actualizacionOpciones={updateOptions}
+            listaProducto={productList}
+        />
 
     )
 
