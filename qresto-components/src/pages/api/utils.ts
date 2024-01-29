16d84a622e35941ec313c5559bc2cbe6fcda92af
JCaeta <63880187+JCaeta@users.noreply.github.com>
2024-01-29 20:56:40 -0300

    Resolver conflictos de merge

--- qresto-components/src/pages/api/utils.ts
+++ qresto-components/src/pages/api/utils.ts
@@ -6,6 +6,23 @@ export function buildDish(data){
     return dish
 }
 
+export function buildSimpleDish(data){
+    const dishes = data.map(dish => {
+        return {
+            id: dish.id,
+            name: dish.name,
+            description: dish.description,
+            isActive: dish.is_active,
+            preparationTime: dish.preparationTime,
+            price: dish.price,
+            restaurant: dish.restaurant,
+            category: dish.category,
+            image: dish.image,
+        }
+    })
+    return dishes
+}
+
 export function buildTableGrid(data){
     const sectors = data.map(s => {
         return {
@@ -48,6 +65,19 @@ export function buildMenu(data){
         }
     })
     return menu
+}
+
+export function buildSideDish(data){
+    const sideDishes = data.map(sideDish => {
+        return {
+            id: sideDish.id,
+            name: sideDish.name,
+            description: sideDish.description,
+            isActive: sideDish.is_active,
+            restaurant: sideDish.restaurant
+        }
+    })
+    return sideDishes
 }
 
 /*
