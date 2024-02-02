a644fe8220ddac2477aac9b43710831333bcc480
Nicolas Rodriguez <mmorodriguez@outlook.com>
2024-02-02 12:17:10 -0300

    eliminar categoria correccion, integracion

--- qresto-restaurant/src/pages/foodmenu/index.tsx
+++ qresto-restaurant/src/pages/foodmenu/index.tsx
@@ -191,6 +191,7 @@ export default function FoodMenuPage() {
             sideDishes={sidedishes}
             deleteSideDish={handleDeleteSideDish}
            // updateDish={handleEditDish} 
+            deleteCategory={deleteCategory}
              />
 
         <FeedbackDialog
