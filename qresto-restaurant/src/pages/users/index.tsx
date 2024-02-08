2d506ad89bc8d8bd202bce20b4acc3ecc4645d4f
Harumi236 <harumi236@proton.me>
2024-02-08 14:02:26 -0300

    Correcciones en User
    Signed-off-by: Harumi236 <harumi236@proton.me>

--- qresto-restaurant/src/pages/users/index.tsx
+++ qresto-restaurant/src/pages/users/index.tsx
@@ -1,13 +1,15 @@
 import {useEffect, useState} from 'react'
 import { Users } from '@/Restaurant/Users/Users'
 import {deleteUser, getUsers, postUser, putUser,} from "@/requests";
+import {getCookieRId} from "qresto-components/src/pages/api/utils";
 
 export default function UsersPage() {
     const [users, setUsers] = useState([])
-    const restaurantID =  1
+    const [restaurantId, setRestaurantId] = useState(1)
     const [userFormOpen, setUserFormOpen] = useState(false)
 
     useEffect(() => {
+        setRestaurantId(getCookieRId)
         fetchUsers()
 
         // Fetch users every 10 seconds
@@ -18,26 +20,23 @@ export default function UsersPage() {
     }, []);
 
     const fetchUsers = async () => {
-        const fetchedUsers = await getUsers(restaurantID)
+        const fetchedUsers = await getUsers(restaurantId)
         setUsers(fetchedUsers)
     }
     const createUser = async (user) => {
         console.log("Usuario?", user)
         const createdUser = await postUser(user)
-        const fetchedUsers = await getUsers(restaurantID)
-        setUsers(fetchedUsers)
+        await fetchUsers()
         return createdUser
     }
     const updateUser = async (user) => {
         const editedUser = await putUser(user)
-        const fetchedUsers = await getUsers(restaurantID)
-        setUsers(fetchedUsers)
+        await fetchUsers()
         return editedUser
     }
     const removeUser = async (userId) => {
         const removedUser = await deleteUser(userId)
-        const fetchedUsers = await getUsers(restaurantID)
-        setUsers(fetchedUsers)
+        await fetchUsers()
         return removedUser
     }
 
